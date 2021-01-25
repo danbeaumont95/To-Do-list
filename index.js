32e409f77acc8fa6e6977031f756897a7de6e6f8
Daniel <danielbeaumont95@hotmail.co.uk>
2021-01-25 16:34:12 +0000

    added strikethrough option on click of list items

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const list = document.getElementById('list');
 
-const addButton = document.getElementById('button').addEventListener('click', addToList)
-
+const addButton = document.getElementById('addButton').addEventListener('click', addToList)
+const removeButton = document.getElementById('removeButton').addEventListener('click', removeFromList)
 
 function addToList() {
-    //console.log('hi from button')
+
 let addedListItem = document.getElementById('addedListItem').value;
 let entry = document.createElement('li');
 entry.appendChild(document.createTextNode(addedListItem))
+entry.onclick= toggle;
 list.appendChild(entry)
+
+}
+
+function removeFromList() {
+    list.removeChild(list.childNodes[0])
+}
+
+
+const checklist = document.querySelectorAll('li')
+checklist.forEach(item => {
+    item.addEventListener('click', toggle)
+})
+
+function toggle() {
+    event.target.classList.toggle('strikethrough')
+
+if (event.target.classList.contains('strikethrough')) {
+    event.target.className = 'strikethrough'
+}
+// else {
+//     event.target.innerHTML = event.target.classList.replace('strikethrough', '')
+// }
+
 }
